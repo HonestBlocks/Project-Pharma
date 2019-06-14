1f655a34907588f19ed841157349e767522341c7
HonestBlocks <honestblocks@gmail.com>
2019-06-14 22:35:22 +0530

    processor

--- processor/main.py
+++
@@ -1,9 +0,0 @@
-from sawtooth_sdk.processor.core import TransactionProcessor
-from sawtooth_PharmaChain.processor.handler import Medicine
-
-
-def main():
-    processor = TransactionProcessor(url = 'tcp://127.0.0.1:4004')
-    medHandler = Medicine()
-    processor.add_handler(medHandler)
-    processor.start()
