b3016c925b40c26531884668276942491465e835
HarshDonga <harshdonga99@gmail.com>
2019-06-20 23:00:20 +0530

    medicine CLI commit

---
+++ med_exceptions.py
@@ -0,0 +1,2 @@
+class MedException(Exception):
+    pass
--- processor/main.py
+++ processor/main.py
@@ -1,4 +1,16 @@
+
+import sys
+import os
+import argparse
+import pkg_resources
+
 from sawtooth_sdk.processor.core import TransactionProcessor
+from sawtooth_sdk.processor.log import init_console_logging
+from sawtooth_sdk.processor.log import log_configuration
+from sawtooth_sdk.processor.config import get_log_config
+from sawtooth_sdk.processor.config import get_log_dir
+from sawtooth_sdk.processor.config import get_config_dir
+
 from sawtooth_PharmaChain.processor.handler import Medicine
 
 
